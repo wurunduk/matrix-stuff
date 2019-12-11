bbd618b51eaa7445f8de7b410857857e8dcfce81
Wurunduk <wurunduk@gmail.com>
2019-12-11 17:00:48 +0300

    started multi-threaded algorithm

---
+++ multi-threaded/main.cpp
@@ -0,0 +1,182 @@
+#include "matrix.h"
+#include "matrix_exception.h"
+#include <ctime>
+#include <iostream>
+#include <pthread.h>
+
+void set_fpu_exception_mask (void);
+
+static void ReportError(MatrixException ex){
+	switch (ex){
+		case NO_ERROR:
+		return;
+		case UNKNOWN_ERROR:
+		printf("unknown error happened while working with matrix, error code %d\n", static_cast<int>(ex));
+		return;
+		case NOT_ENOUGH_MEMORY:
+		printf("Could not allocate enough memory for the matrix.\n");
+		return;
+		case CAN_NOT_OPEN:
+		printf("Could not open the matrix file.\n");
+		return;
+		case FILE_CORRUPT:
+		printf("Matrix file did not have enough numbers to fill the given array.\n");
+		return;
+	}
+}
+
+void PrintUsage(const char* executable_name){
+    printf("Usage: %s n m p for auto generated matrix of size n with block size m and p threads\n %s n m p 'file' to read matrix of size n from file with block size m and p threads\n", executable_name, executable_name);
+}
+
+void InitializeTempAddresses(addresses_array* adr, int* e){
+	
+	e |= Matrix::CreateMatrix(&adr->inverse_block, block_size, block_size);//temp mm blocks
+    e |= Matrix::CreateMatrix(&adr->block, block_size, block_size);
+    e |= Matrix::CreateMatrix(&adr->block_temp, block_size, block_size);
+    e |= Matrix::CreateMatrix(&adr->block_temp_im, block_size, block_size);
+	e |= Matrix::CreateMatrix(&adr->block_temp_sub, block_size, block_size);
+    e |= Matrix::CreateMatrix(&adr->block_me, block_size, end);//temp me blocks
+    e |= Matrix::CreateMatrix(&adr->block_me_temp, block_size, end);
+    e |= Matrix::CreateMatrix(&adr->block_me_temp_im, block_size, end);
+    e |= Matrix::CreateMatrix(&adr->block_me_temp_sub, block_size, end);
+    e |= Matrix::CreateVector(&adr->vector_block, block_size);//temp b vector blocks
+    e |= Matrix::CreateVector(&adr->vector_block_temp, block_size);
+    e |= Matrix::CreateVector(&adr->vector_block_temp_im, block_size);
+	e |= Matrix::CreateMatrix(&adr->block_ee, end, end);//temp ee blocks
+	e |= Matrix::CreateMatrix(&adr->block_ee_temp, end, end);
+	e |= Matrix::CreateVector(&adr->vector_e, end);//temp vector end blocks
+	e |= Matrix::CreateVector(&adr->vector_e_temp, end);
+}
+
+void DeleteTempAddresses(addresses_array* adr){
+	delete[] adr->inverse_block;
+	delete[] adr->block;
+	delete[] adr->block_temp;
+	delete[] adr->block_temp_im;
+	delete[] adr->block_temp_sub;
+	delete[] adr->block_me;
+	delete[] adr->block_me_temp;
+	delete[] adr->block_me_temp_im;
+	delete[] adr->block_me_temp_sub;
+	delete[] adr->vector_block;
+	delete[] adr->vector_block_temp;
+	delete[] adr->vector_block_temp_im;
+	delete[] adr->block_ee;
+	delete[] adr->block_ee_temp;
+	delete[] adr->vector_e;
+	delete[] adr->vector_e_temp;
+}
+
+int main(int argc, char* argv[]){
+    set_fpu_exception_mask();
+    char* file_name = nullptr;
+    int matrix_size = 0;
+    int block_size = 0;
+	int thread_count = 0;
+    int e = 0;				//exceptions count to track errors
+    double *A, *x, *b, *Ax;	
+	arg* args = nullptr;
+	pthread_barrier_t barrier;
+
+    if(!(argc==5 || argc==4) || !(matrix_size = atoi(argv[1])) || !(block_size = atoi(argv[2])) || !(thread_count = atoi(argv[3]))){
+        PrintUsage(argv[0]);
+        return 1;
+    }
+
+    if(argc == 5) file_name = argv[4];
+
+    printf("loading file %s with matrix size %d, block size %d, thread count %d\n", file_name, matrix_size, block_size, thread_count);
+
+	pthread_barrier_init(&barrier, 0, thread_count);
+
+	//size of end block of the matrix
+    int end = matrix_size - (matrix_size/block_size)*block_size;
+	
+    //initialize used variables
+    e |= Matrix::CreateMatrix(&A, matrix_size, matrix_size, file_name);
+    e |= Matrix::CreateVector(&x, matrix_size);
+    e |= Matrix::CreateVector(&b, matrix_size);
+
+    e |= Matrix::CreateVector(&Ax, matrix_size);
+
+    args = new arg[thread_count];
+
+	for(int i = 0; i < thread_count; i++){
+		InitializeTempAddresses(&(args[i]->adr), &e);
+		args[i].matrix = A;
+		args[i].rhs = b;
+		args[i].x = x;
+		args[i].size = matrix_size;
+		args[i].block_size = block_size;
+
+		args[i].return_value = 0;
+
+		args[i].thread_count = thread_count;
+		args[i].thread_id = id;
+		args[i].work_time = 0;
+		args[i].matrix = A;
+
+		args[i].barrier = &barrier;
+	}
+    
+    ReportError((MatrixException)e);
+    if(e != NO_ERROR)
+    {
+        delete[] A;
+        delete[] x;
+        delete[] b;
+        delete[] Ax;
+        
+        for(int i = 0; i < thread_count; i++)
+            DeleteTempAddresses(&(args[i]->adr));
+
+		delete[] args;
+        return 2;
+    }
+
+	Matrix::GetRHSVector(A, b, matrix_size);
+
+    auto t = clock();
+    
+    int res = Matrix::SolveBlock(A, b, x, matrix_size, block_size, temps);
+    
+    t = clock()-t;
+    
+    if(res == 0){
+        //at this point matrix A and vector b are wrong, but we can reinitialize them;
+        Matrix::InitMatrix(A, matrix_size, matrix_size, file_name);
+        Matrix::GetRHSVector(A, b, matrix_size);
+        
+        Matrix::MultiplyMatrices(A, x, Ax, matrix_size, matrix_size, 1);
+        
+        printf("Solution found: \n");
+        Matrix::PrintVector(x, matrix_size);
+        printf("Resulting Ax= vector: \n");
+        Matrix::PrintVector(Ax, matrix_size);	
+        printf("Intendent b vector: \n");
+        Matrix::PrintVector(b, matrix_size);
+        
+        double residual = Matrix::LengthVector(Matrix::SubstractMatrices(Ax, b, matrix_size, 1), matrix_size);
+        double error = Matrix::GetError(x, matrix_size);
+
+        printf("Residual=%e Error=%e n=%d m=%d k=%d elapsed=%.2f\n", residual, error, matrix_size, block_size, thread_count, static_cast<float>(t)/CLOCKS_PER_SEC);
+    }
+    else{
+        printf("Matrix could not be inverted.\n");
+    }
+    
+	delete[] A;
+	delete[] x;
+	delete[] b;
+	delete[] Ax;
+    
+    for(int i = 0; i < thread_count; i++)
+    	DeleteTempAddresses(&(args[i]->adr));
+
+	delete[] args;
+
+	pthread_barrier_destroy(&barrier);
+    
+    return 0;
+}
---
+++ multi-threaded/matrix.h
@@ -0,0 +1,83 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#include "matrix_exception.h"
+#include <memory>
+
+typedef struct{
+	double* inverse_block;
+	double* block;
+    double* block_temp;
+    double* block_temp_im;
+    double* block_temp_sub;
+    double* block_me;
+    double* block_me_temp;
+    double* block_me_temp_im;
+    double* block_me_temp_sub;
+    double* vector_block;
+    double* vector_block_temp;
+    double* vector_block_temp_im;
+	double* block_ee;
+	double* block_ee_temp;
+	double* vector_e;
+	double* vector_e_temp;
+} address_array;
+
+typedef struct{
+    double* matrix;
+    double* rhs;
+	double* answer;
+	int size;
+	int block_size;
+
+	int return_value;
+
+	int thread_count;
+	int thread_id;
+
+	addresses_array adr;
+    
+    double work_time;
+    
+    pthread_barrier_t* barrier;
+} arg;
+
+namespace Matrix{
+	void FillMatrix(double* matrix, const int w, const int h);	
+	void GetAnswerVector(double* vector, const int size);
+    void PrintClean(const double* matrix, const int w, const int h);
+    void Print(const double* matrix, const int size, int print_size = 10);
+	void Print(const double* matrix, const int size, const int* indexes, int print_size = 10);
+	void PrintVector(const double* vector, const int size, int print_size = 10);
+	void PrintVector(const double* vector, const int size, const int* indexes, int print_size = 10);
+	double Length(const double* matrix, const int w, const int h);
+	double LengthVector(const double* vector, const int size);
+    
+    void NullMatrix(double* matrix, const int size);
+	void EMatrix(double* matrix, const int size);
+    MatrixException ReadMatrix(double* matrix, const int w, const int h, const char* file_name);
+    MatrixException InitMatrix(double* matrix, const int w, const int h, const char* file_name);
+
+	void GetRHSVector(const double* matrix, double* RHSVector, const int size);
+    
+    void GetBlock(const double* A, double* block, const int x, const int y, const int x1, const int y1, const int matrix_size);
+    void PutBlock(double* A, const double* block, const int x, const int y, const int x1, const int y1, const int matrix_size);
+
+	MatrixException CreateVector(double** vector, const int size);
+    MatrixException CreateMatrix(double** matrix, const int w, const int h);
+    MatrixException CreateMatrix(double** matrix, const int w, const int h, const char* file_name);
+
+	int GetInverseMatrix(double* matrix, double* matrixReversed, int m, double norm, int* transposition_m);
+
+	double* MultiplyMatrices(double* matrix1, double* matrix2, double* res, const int h, const int wh, const int w);
+	double* MultiplyMatricesNMK(double* matrix1, double* matrix2, double* res, const int h, const int wh, const int w);
+	void MultiplyMatricesNN_4(double* a, double* b, double* c, const int m);
+	void MultiplyMatricesNN(double* a, double* b, double* c, const int n);
+    double* SubstractMatrices(double* matrix, const double* matrix2, const int w, const int h);
+    
+    double GetError(double* vector, const int size);
+
+	int SolveBlock(double* matrix, double* rhs, double* answer, const int size, const int block_size, double* array[]);
+}
+
+#endif

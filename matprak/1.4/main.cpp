10bf4424ee96d5c3b1c67d41dd785cd6f46c58cb
Wurunduk <wurunduk@gmail.com>
2019-12-25 19:53:58 +0300

    Fixed matprak 1

--- matprak/1.4/main.cpp
+++ matprak/1.4/main.cpp
@@ -7,10 +7,9 @@ typedef struct{
     int total_threads;
     int max_count;
     int local_max;
-    int found_max;
+    int did_find_element;
     int* answer;
     int return_value;
-    pthread_barrier_t* barrier;
 } arg;
 
 
@@ -22,12 +21,8 @@ void* thread_function(void* in){
     FILE* f = fopen(args->file_name, "r");
     if(!f) {
         args->return_value=-1;
-        pthread_barrier_wait(args->barrier);
         return 0;
     }
-    
-    int p = args->total_threads;
-    int id = args->thread_id;
     
     double cur = 0;
     double max;
@@ -38,59 +33,37 @@ void* thread_function(void* in){
     if(k == 1){
         max = cur;
         max_count = 1;
-        args->found_max = 1;
+        args->did_find_element = 1;
         
         while(fscanf(f, "%lf ", &cur) == 1){
             if(cur > max){
                 max = cur;
                 max_count = 1;
             }
+			else if(cur == max){
+				max_count += 1;
+			}
         }
         if(!feof(f)){
             args->return_value = -2;
             printf("Thread %d could not parse a number!\n", args->thread_id);
             fclose(f);
-            pthread_barrier_wait(args->barrier);
             return 0;
         }
     }
     else{
         args->max_count = 0;
-        args->found_max = 0;
+        args->did_find_element = 0;
         //save error value of the current threads, 
         //fill the berriers so other threads could finish calculations
         //args->return_value = -2;
         printf("Thread %d had no numbers!\n", args->thread_id);
         fclose(f);
-        pthread_barrier_wait(args->barrier);
         return 0;
     }
     
     args->max_count = max_count;
     args->local_max = max;
-    
-    //wait for the sync before continuing
-    pthread_barrier_wait(args->barrier);
-    
-    int new_max = 0;
-    int found_max_total = 0;
-    
-    if(id == 0){
-        for(int i = 0; i < p; i++){
-            if((args+i)->found_max == 1){
-                if((args+i)->local_max == new_max){
-                    found_max_total += (args+i)->max_count;
-                }
-                else if((args+i)->local_max > new_max){
-                    new_max = (args+i)->local_max;
-                    found_max_total = (args+i)->max_count;
-                }
-            }
-        }
-        
-        *(args->answer) = found_max_total;
-    }
-    
     fclose(f);
     return 0;
 }
@@ -103,35 +76,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     
-    //used by threads for syncing answers and longest arrays
     int answer = 0;
-    
-    pthread_barrier_t barrier;
     
     pthread_t* threads = new pthread_t[files_num];
     arg* args = new arg[files_num];
     
-    pthread_barrier_init(&barrier, 0, files_num);
-    /*
-     * typedef struct{
-    char* file_name;
-    int thread_id;
-    int total_threads;
-    int max_count;
-    int local_max;
-    int* answer;
-    pthread_barrier_t* barrier;
-    } arg;*/
     for(int i = 0; i < files_num; i++){
         args[i].thread_id = i;
         args[i].total_threads = files_num;
         args[i].file_name = argv[i+1];
         args[i].answer = &answer;
         args[i].local_max = 0;
-        args[i].found_max = 0;
+        args[i].did_find_element = 0;
         args[i].max_count = 0;
         args[i].return_value = 0;    
-        args[i].barrier = &barrier;
         if(pthread_create(&threads[i], 0, &thread_function, args+i))
         {
             delete[] threads;
@@ -142,18 +100,18 @@ int main(int argc, char* argv[]) {
     }
     
     int error = 0;
-    int found_files = 0;
+    int found_files_amount = 0;
     for(int i = 0; i < files_num; i++){
         pthread_join(threads[i], 0);
-        found_files += args[i].found_max;
+        found_files_amount += args[i].did_find_element;
+		answer += args->max_count;
         printf("Return value of thread %d is %d\n", i, args[i].return_value);
         if(args[i].return_value < 0) error = args[i].return_value;
     }
-    if(!error && found_files > 0) printf("Answer %d\n", answer);
-    else if(found_files == 0) printf("No numbers were found in any files\n");
+
+    if(!error && found_files_amount > 0) printf("Answer %d\n", answer);
+    else if(found_files_amount == 0) printf("No numbers were found in any file\n");
     else printf("Error %d occured\n", error);
-    
-    pthread_barrier_destroy(&barrier);
     
     delete[] threads;
     delete[] args;
